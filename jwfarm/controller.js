9fff4cba4c8fd34fdb899231b214fda4cd6dd11d
exagent957 <exagent957@gmail.com>
2022-11-13 18:49:22 -0600

    111322a

--- jwfarm/controller.js
+++ jwfarm/controller.js
@@ -3,7 +3,7 @@ import { getWeather, getEvents } from "./model.js";
 import { renderWeather } from "./view.js";
 
 const apiKey = "24bf56ca08654dbfbf56ca0865adbf49";
-const apiUrl = `https://api.weather.com/v2/pws/observations/current?stationId=KALHARTS33&format=json&units=e&apiKey=${apiKey}&numericPrecision=decimal`;
+const apiUrl = `https://api.weather.com/v2/pws/observations/current?stationId=KALHARTS51&format=json&units=e&apiKey=${apiKey}&numericPrecision=decimal`;
 const jsonUrl = "./farmEvents.json";
 
 export function showWeather(apiUrl) {
@@ -17,6 +17,11 @@ showWeather(apiUrl);
 export function showEvents(jsonUrl) {
   getEvents(jsonUrl).then((events) => {
     console.log(events);
+    let today = new Date();
+    console.log({ today });
+    events.forEach((event) => {
+      event.date = new Date(event.date);
+    });
   });
 }
 showEvents(jsonUrl);
